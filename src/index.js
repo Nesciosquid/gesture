e014ede93c6a8783d239fe84110a8614b393832f
Aaron Heuckroth <a.heuckroth@gmail.com>
2017-11-17 15:52:45 -0500

    For reals though, stop scrolling

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+document.body.addEventListener('touchmove', (e) => e.preventDefault());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
