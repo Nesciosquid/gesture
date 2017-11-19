4ccbada243643a938f8ea8be59d44a1df81e18c8
Aaron Heuckroth <a.heuckroth@gmail.com>
2017-11-19 00:06:55 -0500

    Add react-router-redux

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,32 @@ import logger from 'redux-logger';
 import reducer from './reducers/';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { routerMiddleware, ConnectedRouter, push } from 'react-router-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 
-let middleware = [promiseMiddleware];
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
+
+import { Route } from 'react-router';
+
+let middleware = [promiseMiddleware, routerMiddleware(history)];
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
-const store = createStore(reducer, applyMiddleware(promiseMiddleware, logger));
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedApp />
+  <ConnectedRouter history={history}>
+    <div>
+      <Route exact={true} path="/" component={ConnectedApp} />
+      <Route path="/foo" component={ConnectedApp} />
+    </div>
+  </ConnectedRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
 
+store.dispatch(push('/foo'));
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,13 +1,16 @@
 import { combineReducers } from 'redux';
 import images, { ImagesState } from './images';
 import settings, { SettingsState } from './settings';
+import { routerReducer, RouterState } from 'react-router-redux';
 
 export interface ReduxState {
     images: ImagesState;
     settings: SettingsState;
+    router: RouterState;
 }
 
 export default combineReducers<ReduxState>({
     images,
-    settings
+    settings,
+    router: routerReducer
 });
