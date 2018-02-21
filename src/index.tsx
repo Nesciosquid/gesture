2b12aedc9cee05dd92b1194cb5518ba9efb18116
Aaron Heuckroth <a.heuckroth@gmail.com>
2018-02-21 10:18:21 -0500

    Move drawing app to separate route

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,19 @@ import logger from 'redux-logger';
 import reducer from './reducers/';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { routerMiddleware } from 'react-router-redux';
+import { routerMiddleware, ConnectedRouter } from 'react-router-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import createHistory from 'history/createBrowserHistory';
-//import { fetchAlbumFromImgur } from './actions/images';
-//import albums from './utils/defaultAlbums';
-// import CanvasExample from './CanvasExample';
+import { fetchAlbumFromImgur } from './actions/images';
+import albums from './utils/defaultAlbums';
+import App from './ConnectedApp';
+import CanvasExample from './CanvasExample';
 import * as Pressure from 'pressure';
 import { setChange } from './actions/pressure';
-import { setContext, startDrawing, stopDrawing, drawFromPressure, setSourceImage, setDrawColor } from './actions/canvas';
-import { TouchEvent, MouseEvent } from 'react';
+import { setSourceImage, setDrawColor, setContext } from './actions/canvas';
 import * as ReduxThunk from 'redux-thunk';
 const pencilSource = require('./utils/pencilSource.png');
+import { Switch, Route } from 'react-router-dom';
 
 const history = createHistory();
 
@@ -26,52 +27,24 @@ if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 const store = createStore(reducer, applyMiddleware(...middleware));
+albums.forEach(album => store.dispatch(fetchAlbumFromImgur(album) as any)); //tslint:disable-line
+Pressure.set('.pressure', {
+  change: (force: number, event: PointerEvent) => { //tslint:disable-line
+    store.dispatch(setChange(force, event));
+  },
+  end: () => {
+    store.dispatch(setChange(0, null));
+  }
+});
 
 ReactDOM.render(
   <Provider store={store}>
-    <div 
-      onTouchStart={(event: TouchEvent<HTMLDivElement>) => {
-        const touch = event.touches[0];
-        const x = touch.clientX;
-        const y = touch.clientY;
-        store.dispatch(startDrawing(x, y));
-      }}
-      onTouchEnd={(event: TouchEvent<HTMLDivElement>) => {
-        store.dispatch(stopDrawing);
-      }}
-      onTouchMove={(event: TouchEvent<HTMLDivElement>) => {
-        const touch = event.touches[0];
-        const x = touch.clientX;
-        const y = touch.clientY;
-        store.dispatch(drawFromPressure(x, y, 10, 50, .2, 1));
-      }}
-      onMouseDown={(event: MouseEvent<HTMLDivElement>) => {
-        const x = event.clientX;
-        const y = event.clientY;
-        store.dispatch(startDrawing(x, y));
-      }}
-      onMouseUp={(event: MouseEvent<HTMLDivElement>) => {
-        store.dispatch(stopDrawing());
-      }}      
-      onMouseMove={(event: MouseEvent<HTMLDivElement>) => {
-        const x = event.clientX;
-        const y = event.clientY;
-        store.dispatch(drawFromPressure(x, y, 10, 50, .2, 1));        
-      }}
-      style={{width: '100%', height: '100%'}}
-    >
-      <div 
-        className="pressure" 
-        style={{
-          width: '100%', 
-          height: '100%', 
-          position: 'absolute', 
-          background: 'none', 
-          zIndex: 99 
-        }} 
-      />
-      <canvas id="canvas" style={{width: '100%', height: '100%' }}/>
-    </div>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact={true} path="/" component={App} />
+        <Route path="/draw" component={CanvasExample} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
@@ -92,18 +65,10 @@ sourceImage.onload = () => {
 };
 sourceImage.src = pencilSource;
 store.dispatch(setDrawColor({
-  r: 0,
-  g: 1,
-  b: 0,
+  r: 1,
+  g: .2,
+  b: .5,
   a: .0
 }));
 
-Pressure.set('.pressure', {
-  change: (force: number, event: PointerEvent) => { //tslint:disable-line
-    store.dispatch(setChange(force, event));
-  },
-  end: () => {
-    store.dispatch(setChange(0, null));
-  }
-});
-//albums.forEach(album => store.dispatch(fetchAlbumFromImgur(album) as any)); //tslint:disable-line
+
